b5a49390d4b7a5968bfec529f4ee92f208957506
ottoalvarez <dev.alvarezclaudio@gmail.com>
2023-02-18 22:56:39 -0300

    Integration of cors module, exceptions for free endpoints and another configs

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-import dotenv from "dotenv";
-import express from "express";
-import morgan from "morgan";
-import fs from "fs";
-import path from "path";
-import moment from "moment";
+import { dotenv, express, morgan, fs, path, moment, cors, expressjwt } from "./src/config/index.js";
+import { corsDomains } from "./src/config/cors.js";
+import { exceptions } from "./src/routes/exceptions.js";
 
 dotenv.config();
 
+// Init Server
 const server = express();
 const port = (process.env.DEBUGGIN_MODE == 'active' ? process.env.SERVERPORT : process.env.LOCALPORT);
 
+// Security Configure
+server.use(cors((req, callback) => {
+    callback(null, { origin: (corsDomains.indexOf(req.header('Origin')) !== -1 ? true : false) });
+}))
+
+// Server Configure 
 const accessLogs = fs.createWriteStream(
-    path.join('./logs/', `request-${moment().format('MMMMYYYY')}.log`), { flags: 'a', interval: '1d' }
+    path.join('./src/logs/', `request-${moment().format('MMMMYYYY')}.log`), { flags: 'a', interval: '1d' }
 );
-
 server.use(morgan('combined', { stream: accessLogs }));
-server.get('/', (req, res) => {
-    res.send('Hi');
-})
+server.use(express.urlencoded({ extended: true }))
 
+// Deploy server
+server.use(
+    '/v1',
+    expressjwt({ secret: process.env.KEY, algorithms: ['HS256'] }).unless({ path: exceptions }),
+    (req, res) => {
+        res.send('HelloWorld');
+    }
+)
 server.listen(port, () => {
     console.log(`Express launched on port [${port}] `)
 })
---
+++ src/config/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import fs from "fs";
+import path from "path";
+import moment from "moment";
+import cors from "cors";
+import { expressjwt } from "express-jwt";
+
+export {
+    dotenv,
+    express,
+    morgan,
+    fs,
+    path,
+    moment,
+    cors,
+    expressjwt
+}
