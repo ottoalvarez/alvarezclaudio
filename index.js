a41ef3c1de3bfd36d96e0a1aee3d1908718952cc
ottoalvarez <dev.alvarezclaudio@gmail.com>
2023-02-18 21:41:52 -0300

    \ No newline at end of fileAdd .gitignore, index.js, .yarn-integrity, and update package.json

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import morgan from "morgan";
+import fs from "fs";
+import path from "path";
+import moment from "moment";
+
+const server = express();
+const port = 3000
+
+const accessLogs = fs.createWriteStream(path.join('./logs/', `request-${moment().format('MMMMYYYY')}.log`), { flags: 'a', interval: '1d' });
+server.use(morgan('combined', { stream: accessLogs }));
+
+server.get('/', (req, res) => {
+    res.send('Hi');
+})
+
+server.listen(port, () => {
+    console.log("first")
+})
